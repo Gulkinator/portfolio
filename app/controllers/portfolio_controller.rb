3e26cb7e139fcfdc72533c65b6567611f621c07f
Gulkinator <lgulkus@gmail.com>
2015-06-23 20:29:32 -0500

    trying to debug email error

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -8,4 +8,11 @@ class PortfolioController < ApplicationController
   def contact
   end
 
+  def send_form
+
+    ContactMailer.email_contact(params).deliver
+    redirect_to action: index, notice: 'Your list has been sent!'
+  end
+
+
 end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,3 +1,16 @@
 class ContactMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "lauren.invox@gmail.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.email_contact.subject
+  #
+  def email_contact(params)
+    @first_name = params['first_name']
+    @last_name = params['last_name']
+    @email = params['email']
+    @message = params['msg']
+    mail to: 'lauren.invox@gmail.com'
+  end
 end
